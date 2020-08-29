c2b3de63528a6422229549c12428156f81637edd
LJ Johnson <lj020326@gmail.com>
2020-08-29 18:52:15 -0400

    updates

--- image/docker-jenkins-agent/entry_point.sh
+++ image/docker-jenkins-agent/entry_point.sh
@@ -3,22 +3,22 @@
 # source: https://github.com/cloudbees/java-build-tools-dockerfile/blob/master/entry_point.sh
 
 echo "jenkins user and group adjustments"
-JENKINS_UID=${JENKINS_UID:-1000}
-JENKINS_GID=${JENKINS_GID:-1000}
+JENKINS_UID=${uid:-1000}
+JENKINS_GID=${gid:-1000}
 
 echo "get current jenkins uid/gid info inside container"
 CUR_USER_GID=$(id -g jenkins || true)
 CUR_USER_UID=$(id -u jenkins || true)
 
 JENKINS_UIDGID_CHANGED=false
-if [ "$JENKINS_UID" != "$CUR_USER_UID" ]; then
-  echo "CUR_USER_UID (${CUR_USER_UID}) does't match JENKINS_UID (${JENKINS_UID}), adjusting..."
-  usermod -o -u "$JENKINS_UID" jenkins
+if [ "$uid" != "$CUR_USER_UID" ]; then
+  echo "CUR_USER_UID (${CUR_USER_UID}) does't match uid (${uid}), adjusting..."
+  usermod -o -u "$uid" jenkins
   JENKINS_UIDGID_CHANGED=true
 fi
-if [ "$JENKINS_GID" != "$CUR_USER_GID" ]; then
-  echo "CUR_USER_GID (${CUR_USER_GID}) does't match JENKINS_GID (${JENKINS_GID}), adjusting..."
-  groupmod -o -g "$JENKINS_GID" jenkins
+if [ "$gid" != "$CUR_USER_GID" ]; then
+  echo "CUR_USER_GID (${CUR_USER_GID}) does't match gid (${gid}), adjusting..."
+  groupmod -o -g "$gid" jenkins
   JENKINS_UIDGID_CHANGED=true
 fi
 
